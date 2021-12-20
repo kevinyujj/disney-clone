5102cf506ca57878710ddab1ffbdecaa1506d2c2
yu950222 <yu950222@gmail.com>
2021-12-20 15:15:20 -0800

    Add a carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,6 +44,7 @@ const Nav = styled.nav`
     display: flex;
     align-items: center;
     padding: 0 36px;
+    overflow-x: hidden;
 `
 
 const Logo = styled.img`
@@ -96,6 +97,7 @@ const NavMenu = styled.div`
 `
 
 const UserImg = styled.img`
+    display: flex;
     width: 48px;
     height: 48px;
     border-radius: 50%;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
+
 
 function Home() {
     return (
-        <div>
-            home
-        </div>
+        <Container>
+            <ImgSlider />
+            <Viewers />
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+    overflow-x: hidden;
+
+    &:before {
+        background: url("/images/home-background.png") center center / cover
+        no-repeat fixed;        
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+`
+
+
+// import React from 'react'
+// import styled from 'styled-components'
+// import ImgSlider from './ImgSlider'
+// function Home() {
+//     return (
+//         <Container >
+//             home
+//             <ImgSlider />
+//         </Container>
+//     )
+// }
+
+// export default Home
+
+
+// //main is better than div for SEO?
+// const Container = styled.main`
+//     min-height: calc(100vh - 70px);
+//     padding: 0 calc(3.5vw + 5px);
+//     position: relative;
+//     background: green url('/images/home-background.png') center center / cover no-repeat fixed;
+//     top: 0;
+//     left: 0;
+//     right: 0;
+//     bottom: 0;
+// `
